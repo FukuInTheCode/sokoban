00b55517c659ecd00b795d184f56caa1dabdfb90
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:46:54 +0100

    [+] Add the main func.

---
+++ my_sokoban.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** Unname file ^^
+** File description:
+** Placeholder
+*/
+
+#include "my.h"
+
+int main(int argc, char **argv)
+{
+    int error = 0;
+
+    return error;
+}
