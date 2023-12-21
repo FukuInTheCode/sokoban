59128ee2f3eb04cb239d662523188ac9355f413a
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:47:14 +0100

    [~] Can move the boxes up and down

--- handle_player.c
+++ handle_player.c
@@ -7,6 +7,29 @@
 
 #include "my.h"
 
+static int handle_box(char *map, int id, int to , int type)
+{
+    if (map[to] != 'X')
+        return 0;
+    if (to - id == 1 && type == 0 && map[to + 1] == ' ') {
+        map[to + 1] = 'X';
+        return 0;
+    }
+    if (to - id == -1 && type == 0 && map[to - 1] == ' ') {
+        map[to - 1] = 'X';
+        return 0;
+    }
+    if (to - id > 1 && type == 1 && map[find_y(map, to, 1)] == ' ') {
+        map[find_y(map, to, 1)] = 'X';
+        return 0;
+    }
+    if (to - id < -1 && type == 1 && map[find_y(map, to, -1)] == ' ') {
+        map[find_y(map, to, -1)] = 'X';
+        return 0;
+    }
+    return 1;
+}
+
 static int find_player(char *map)
 {
     int id = 0;
@@ -19,7 +42,10 @@ static int move_x(char *map, int dx, char *base_map)
 {
     int id = find_player(map);
 
-    if ((map[id + dx] != ' ' && map[id + dx] != 'O') || dx == 0)
+    if ((map[id + dx] != ' ' && map[id + dx] != 'O'
+        && map[id + dx] != 'X') || dx == 0)
+        return 0;
+    if (handle_box(map, id, id + dx, 0))
         return 0;
     map[id + dx] = 'P';
     map[id] = base_map[id];
@@ -33,7 +59,9 @@ static int move_y(char *map, int dy, char *base_map)
     int id = find_player(map);
     int to = find_y(map, id, dy);
 
-    if ((map[to] != ' ' && map[to] != 'O') || dy == 0)
+    if ((map[to] != ' ' && map[to] != 'O' && map[to] != 'X') || dy == 0)
+        return 0;
+    if (handle_box(map, id, to, 1))
         return 0;
     map[to] = 'P';
     map[id] = base_map[id];
