c30a0eac42394f320349e707ee293fb6ed44ab4e
Tom C. <tom.carlier@epitech.eu>
2023-12-21 19:46:52 +0100

    [+] add the header file

---
+++ include/my.h
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2023
+** MY_H
+** File description:
+** Placeholder
+*/
+
+#ifndef MY_H
+    #define MY_H
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <time.h>
+    #include <limits.h>
+    #include <stdarg.h>
+    #include <errno.h>
+    #include <fcntl.h>
+    #include <math.h>
+    #include <stdlib.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <sys/sysmacros.h>
+    #include <dirent.h>
+    #include <pwd.h>
+    #include <ncurses.h>
+    #include <grp.h>
+    #include <criterion/criterion.h>
+    #include <criterion/redirect.h>
+
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Config.h>
+    #include <SFML/GPUPreference.h>
+    #include <SFML/Network.h>
+    #include <SFML/OpenGL.h>
+    #include <SFML/System.h>
+    #include <SFML/Window.h>
+
+    #include "../lib/my_printf/include/my.h"
+
+#endif
